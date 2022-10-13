c1427010d30cb1f37f1dbf836946cfd41f06f490
John W <john.weaver94@utexas.edu>
2022-10-13 17:04:12 -0500

    had the year on my work day scheduler and it didn't need that.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
 var save = document.querySelector(".saveBtn")
 //Sets a variable for creating the time, and then linking it back to the html so it shows up!
-var time = moment().format('dddd MMMM Do YYYY');
+var time = moment().format('dddd MMMM Do');
 $("#currentDay").text(time);
