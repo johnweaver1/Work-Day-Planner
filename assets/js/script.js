ecf01d0952a3d7a9db71874110e5599ad8bb22a5
John W <john.weaver94@utexas.edu>
2022-10-14 19:29:30 -0500

    made the buttons save the text in text area. added refrence so when the screen is refreshed the text that was previously saved will be re-written.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,27 @@
-var save = document.querySelector(".saveBtn")
+
 //Sets a variable for creating the time, and then linking it back to the html so it shows up!
 var time = moment().format('dddd MMMM Do');
 $("#currentDay").text(time);
+// when clicking the save button your text in the text area class is saved by the id in the div corresponding to the hour.
+$(".saveBtn").on("click",function(){
+
+   var words = $(this).siblings(".beepo").val();
+
+   var id = $(this).parent().attr("id");
+   
+   localStorage.setItem(id, words);
+})
+//bringing back the local storage to the text area from using the save button.
+$("#9 .beepo").val(localStorage.getItem("9"));
+$("#10 .beepo").val(localStorage.getItem("10"));
+$("#11 .beepo").val(localStorage.getItem("11"));
+$("#12 .beepo").val(localStorage.getItem("12"));
+$("#13 .beepo").val(localStorage.getItem("13"));
+$("#14 .beepo").val(localStorage.getItem("14"));
+$("#15 .beepo").val(localStorage.getItem("15"));
+$("#16 .beepo").val(localStorage.getItem("16"));
+$("#17 .beepo").val(localStorage.getItem("17"));
+
 //function for changing the text area's color depending on the time!
 function color() {
     // variable for setting the hour
@@ -19,5 +39,6 @@ function color() {
         }
     })
 };
-
+var hour = moment().hours();
+console.log(hour)
 color ();
